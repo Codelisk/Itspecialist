210a96f1d6ec25dc2ee6dde19b5ef49d9d845164
Daniel Hufnagl <d.hufnagl@codelisk.com>
2023-11-08 14:44:08 +0100

    WIP

--- Itspecialist/Backend/Itspecialist.Server/Program.cs
+++ Itspecialist/Backend/Itspecialist.Server/Program.cs
@@ -1,16 +1,10 @@
 using System.Text.Json.Serialization.Metadata;
-using Itspecialist.DataContracts.Serialization;
 using Uno.Wasm.Bootstrap.Server;
 
 try
 {
     var builder = WebApplication.CreateBuilder(args);
 
-    // Configure the JsonOptions to use the generated WeatherForecastContext
-    builder.Services.Configure<JsonOptions>(options =>
-        options.JsonSerializerOptions.TypeInfoResolver = JsonTypeInfoResolver.Combine(
-            WeatherForecastContext.Default
-        ));
     // Configure the RouteOptions to use lowercase URLs
     builder.Services.Configure<RouteOptions>(options =>
         options.LowercaseUrls = true);
@@ -43,7 +37,6 @@ try
     app.UseUnoFrameworkFiles();
     app.MapFallbackToFile("index.html");
 
-    app.MapWeatherApi();
     app.UseStaticFiles();
 
     await app.RunAsync();
