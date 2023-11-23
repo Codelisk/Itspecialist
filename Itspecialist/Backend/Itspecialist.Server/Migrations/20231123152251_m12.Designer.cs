ff7e8c83d8f52f9769ad8b598f29e2785c354698
Daniel Hufnagl <d.hufnagl@codelisk.com>
2023-11-23 16:23:09 +0100

    Migrations

--- Itspecialist/Backend/Itspecialist.Server/Migrations/20231123152251_m12.Designer.cs
+++ Itspecialist/Backend/Itspecialist.Server/Migrations/20231123152251_m12.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Itspecialist.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Itspecialist.Server.Migrations
 {
     [DbContext(typeof(ItspecialistContext))]
-    partial class ItspecialistContextModelSnapshot : ModelSnapshot
+    [Migration("20231123152251_m12")]
+    partial class m12
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.0");
---
+++ Itspecialist/Backend/Itspecialist.Server/Migrations/20231123152251_m12.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Itspecialist.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class m12 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "talentCompensationEntity",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateTable(
+                name: "favoriteOpportunityEntity",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    UserId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    AccountId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    CareerOpportunityId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_favoriteOpportunityEntity", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "favoriteTalentEntity",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    UserId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    AccountId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    TalentProfileId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_favoriteTalentEntity", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "favoriteOpportunityEntity");
+
+            migrationBuilder.DropTable(
+                name: "favoriteTalentEntity");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "talentCompensationEntity");
+        }
+    }
+}
